7408de5fc149b9c1e28e1caa8d08cf5517364833
vigier0709 <mev.cae@gmail.com>
2019-03-13 19:58:43 +0900

    Trixエディターに画像をペーストできる

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,3 +18,40 @@
 //= require bootstrap-sprockets
 //= require turbolinks
 //= require_tree .
+
+function uploadAttachment(attachment) {
+
+  var file = attachment.file;
+  var form = new FormData;
+  form.append("Content-Type", file.type);
+  form.append("recruitment[image]", file);
+
+  var xhr = new XMLHttpRequest;
+  xhr.open("Recruitment", "/recruitments.json", true);
+  xhr.setRequestHeader("X-CSRF-Token", Rails.csrftoken());
+
+  xhr.upload.onprogress = function(event) {
+    var progress = event.loaded / event.total * 100;
+    attachment.setUploadProgress(progress);
+  }
+
+  xhr.onload = function() {
+    if (xhr.status === 201) {
+      var data = JSON.parse(xhr.responseText);
+      return attachment.setAttributes({
+        url: data.image_url,
+        href: data.url
+      })
+    }
+  }
+
+  return xhr.send(form);
+}
+
+// Listen for the Trix attachment event to trigger upload
+document.addEventListener("trix-attachment-add", function(event) {
+  var attachment = event.attachment;
+  if (attachment.file) {
+    return uploadAttachment(attachment);
+  }
+});
