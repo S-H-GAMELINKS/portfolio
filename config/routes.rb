821f8cccdefd6fa85c6038484f450a3a10240df8
vigier0709 <mev.cae@gmail.com>
2019-03-14 13:50:13 +0900

    scaffoldでtopicsのCRUDを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :topics
   resources :photos
   get 'users/show'
   get 'users/index'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_13_143008) do
+ActiveRecord::Schema.define(version: 2019_03_14_044850) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,6 +51,14 @@ ActiveRecord::Schema.define(version: 2019_03_13_143008) do
     t.bigint "category_id"
     t.string "category"
     t.index ["category_id"], name: "index_recruitments_on_category_id"
+  end
+
+  create_table "topics", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.date "startdate"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
