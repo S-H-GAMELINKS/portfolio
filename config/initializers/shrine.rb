5f5d879fe15a492bc55e0ae83fed9bbde2c8eade
vigier0709 <mev.cae@gmail.com>
2019-03-15 21:53:14 +0900

    アップロード画像をリサイズするためのgemを導入

--- config/initializers/shrine.rb
+++ config/initializers/shrine.rb
@@ -1,10 +1,27 @@
-require "shrine"
-require "shrine/storage/file_system"
+require 'shrine'
+require 'shrine/storage/file_system'
+# require 'shrine/storage/s3'
 
-Shrine.storages = {
-  cache: Shrine::Storage::FileSystem.new("public", prefix: "uploads/cache"),
-  store: Shrine::Storage::FileSystem.new("public", prefix: "uploads/store"),
-}
+# アップロードするディレクトリの指定
+# if Rails.env.production?
+#   s3_options = {
+#       access_key_id:     'your_access_key_id',
+#       secret_access_key: 'your_secret_access_key',
+#       region:            'ap-northeast-1',
+#       bucket:            'abc.example.com',
+#   }
+ 
+#   Shrine.storages = {
+#       cache: Shrine::Storage::S3.new(prefix: 'cache', **s3_options),
+#       store: Shrine::Storage::S3.new(prefix: 'store', **s3_options),
+#   }
+# else
+  Shrine.storages = {
+      cache: Shrine::Storage::FileSystem.new('public', prefix: 'uploads/cache'),
+      store: Shrine::Storage::FileSystem.new('public', prefix: 'uploads/store'),
+  }
+# end
 
+# 使用するプラグインの宣言
 Shrine.plugin :activerecord
-Shrine.plugin :cached_attachment_data
+Shrine.plugin :cached_attachment_data # for_forms
