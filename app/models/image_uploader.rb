22446e8b67207dc5116803cbb6596f68abc24f4a
vigier0709 <mev.cae@gmail.com>
2019-03-15 21:59:22 +0900

    Uploaderに画像リサイズの処理を追加

--- app/models/image_uploader.rb
+++ app/models/image_uploader.rb
@@ -1,2 +1,10 @@
+require 'image_processing/mini_magick'
+
 class ImageUploader < Shrine
+  incluede ImageProcessing::MiniMagick
+  plugin :processing
+
+  process(:store) do |io, context|
+    resize_to_limit!(io.download, 400, 400) { |cmd| cmd.auto_orient }
+  end
 end
