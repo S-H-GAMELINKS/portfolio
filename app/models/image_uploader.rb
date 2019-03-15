746cf53cf0eb4ccb48576f6bf2bd0c27a2b77231
vigier0709 <mev.cae@gmail.com>
2019-03-15 22:47:47 +0900

    versionsとdelete_rawのプラグインを追加し、リサイズ処理を修正

--- app/models/image_uploader.rb
+++ app/models/image_uploader.rb
@@ -3,8 +3,15 @@ require 'image_processing/mini_magick'
 class ImageUploader < Shrine
   incluede ImageProcessing::MiniMagick
   plugin :processing
+  plugin :versions
+  plugin :delete_raw
 
   process(:store) do |io, context|
-    resize_to_limit!(io.download, 400, 400) { |cmd| cmd.auto_orient }
+    original = io.download
+
+    size_400 = resize_to_limit!(original, 400, 400) { |cmd| cmd.auto_orient }
+    size_100 = resize_to_limit(size_400, 100, 100)
+
+    { original: io, large: size_400, thumb: size_100 }
   end
 end
