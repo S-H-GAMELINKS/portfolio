bf174e4ac690e8307e192b3e31bc8fdbf54eb064
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-03-23 07:30:19 +0000

    Migrate DB

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_22_103738) do
+ActiveRecord::Schema.define(version: 2019_03_23_072902) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -76,6 +76,7 @@ ActiveRecord::Schema.define(version: 2019_03_22_103738) do
     t.datetime "updated_at", null: false
     t.string "name"
     t.text "self_introduction"
+    t.text "icon_data"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
